50ff8d100011815ef7aeeeef9544fb62380322e5
Kostiantyn <g00436001@atu.ie>
2025-03-30 23:07:14 +0000

    code
    dified:   src/app/home/home.module.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../services/movie.service';
+import { CommonModule } from '@angular/common'; 
+import { 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonList, 
+  IonItem, 
+  IonAvatar, 
+  IonLabel 
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonList, 
+    IonItem, 
+    IonAvatar, 
+    IonLabel,
+    CommonModule
+  ]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  movies: any[] = [];
 
-  constructor() {}
+  constructor(private movieService: MovieService) {}
 
+  ngOnInit() {
+    this.searchMovies('war');
+  }
+
+  searchMovies(title: string) {
+    this.movieService.searchMovies(title).subscribe((data: any) => {
+      if (data.Search) {
+        this.movies = data.Search;
+      }
+    });
+  }
 }
